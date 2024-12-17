fafe7ff420c9f4be0e0051e04919e2cf600c5730
Annika1188 <annikas_lane@live.com.au>
2024-12-17 19:56:59 +1100

    added js functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateFact(event) {
+  event.preventDefault();
+  new Typewriter("#answer", {
+    strings: "Fun Facts coming....",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
+let factFormElement = document.querySelector("#fact-generator");
+factFormElement.addEventListener("submit", generateFact);
