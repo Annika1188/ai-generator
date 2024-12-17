9adcc5fe826988878d01b14442f2d2948514cf05
Annika1188 <annikas_lane@live.com.au>
2024-12-17 20:26:58 +1100

    added axios and ai api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-function generateFact(event) {
-  event.preventDefault();
+function displayFact(response) {
   new Typewriter("#answer", {
-    strings: "Fun Facts coming....",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateFact(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2t140860597f63afo033b6cda0bf4143";
+  let context =
+    "You are a fun fact expert. You mission is to generate a 1 line fact, the funnier the better, in basic HTML. Make sure to follow the user instructions.  Sign the fact with 'SheCodes AI' inside a <strong> element at the end ßand NOT at the beginning";
+  let prompt = `User instructions: Generate a fun fact about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayFact);
 }
 
 let factFormElement = document.querySelector("#fact-generator");
